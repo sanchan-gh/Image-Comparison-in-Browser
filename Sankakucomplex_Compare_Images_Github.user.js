a568bb2af19034d57f45ca03f9166fcd26bca07f
sanchan <77040593+sanchan-gh@users.noreply.github.com>
2022-02-10 19:25:52 +0100

    update userscript to use new Github link

--- Sankakucomplex_Compare_Images_Github.user.js
+++ Sankakucomplex_Compare_Images_Github.user.js
@@ -1,44 +1,48 @@
 // ==UserScript==
 // @name        Sankakucomplex Compare Images Github
-// @namespace   https://github.com/jklgit/
+// @namespace   https://github.com/sanchan-gh/
 // @description Compare images on github
 // @include     https://chan.sankakucomplex.com/post/show/*
 // @include     https://chan.sankakucomplex.com/post/delete/*
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
-// Add frame do deletion page
-var thumbs = document.querySelectorAll('.deleting-post .thumb');
-if(thumbs.length === 2){
-	
-	// Create an iframe
-	var iframe = document.createElement('iframe');
-	iframe.src = 'https://jklgit.github.io/Image-Comparison-in-Browser/index.html\
-		?sid1=' + thumbs[0].getAttribute('id').substr(1) + '&sid2=' + thumbs[1].getAttribute('id').substr(1);
-	iframe.style = 'height:700px; margin-left: 2em; width: calc(100% - 4em);';
-	
-	// Append iframe
-	document.getElementById('content').appendChild(iframe);
+(function() {
+'use strict';
+
+// Add frame to deletion page
+const thumbs = document.querySelectorAll('.deleting-post .thumb');
+if (thumbs.length === 2) {
+  // Create an iframe
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://sanchan-gh.github.io/Image-Comparison-in-Browser/index.html'
+    + `?sid1=${thumbs[0].getAttribute('id').slice(1)}`
+    + `&sid2=${thumbs[1].getAttribute('id').slice(1)}`;
+  iframe.style.height = '700px';
+  iframe.style.marginLeft = '2em';
+  iframe.style.width = 'calc(100% - 4em)';
+
+  // Append iframe
+  document.getElementById('content').appendChild(iframe);
 }
 
 // Add link to post/show/ page
-var pool = document.getElementById('add-to-pool');
-if(pool){
-	// Compare with parent post if it exists
-	var parentArg = '';
-	var parent = document.querySelector('#parent-preview > .thumb');
-	if(parent){
-		parentArg = '&sid2=' + parent.getAttribute('id').substr(1);
-	}
-	
-	var a = document.createElement('a');
-	a.innerHTML = 'Compare post content';
-	a.target = '_blank';
-	a.href = 'https://jklgit.github.io/Image-Comparison-in-Browser/index.html\
-		?url1=' + encodeURIComponent(document.location.href) + parentArg;
-	
-	var li = document.createElement('li');
-	li.innerHTML = a.outerHTML;
-	pool.parentNode.appendChild(li);
+const pool = document.getElementById('add-to-pool');
+if (pool) {
+  // Compare with parent post if it exists
+  const parent = document.querySelector('#parent-preview > .thumb');
+
+  const a = document.createElement('a');
+  a.innerHTML = 'Compare post content';
+  a.target = '_blank';
+  a.href = 'https://sanchan-gh.github.io/Image-Comparison-in-Browser/index.html'
+    + `?url1=${encodeURIComponent(document.location.href)}`
+    + (parent ? `&sid2=${parent.getAttribute('id').slice(1)}` : '');
+
+  const li = document.createElement('li');
+  li.innerHTML = a.outerHTML;
+  pool.parentNode.appendChild(li);
 }
+
+})();
